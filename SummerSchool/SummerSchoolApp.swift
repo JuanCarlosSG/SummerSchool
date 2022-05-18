464bb931fbd5ea9a6ac5d27a103b67ee250607e0
JuanCarlosSG <jcsagu00@gmail.com>
2022-05-17 20:35:57 -0500

    Initial Commit

---
+++ SummerSchool/SummerSchoolApp.swift
@@ -0,0 +1,17 @@
+//
+//  SummerSchoolApp.swift
+//  SummerSchool
+//
+//  Created by Juan Carlos Sánchez Gutiérrez on 17/05/22.
+//
+
+import SwiftUI
+
+@main
+struct SummerSchoolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
